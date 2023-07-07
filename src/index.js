cd84bfb13d00de31095f4ed010f87e6a999d58d8
whiskacz <pawelblasiak1985@gmail.com>
2023-07-07 20:09:41 +0200

    r3

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import border  from "./img/border.jpg";
+import './index.css'
+import { RxHeart, RxHeartFilled, RxFace} from "react-icons/rx";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const MyFunction = () => {
+
+    const [like, setHeart] = useState(false)
+    const [count, setCount] = useState(0)
+
+    const handleHeart = (e) => {
+        if(like){
+            setHeart(false)
+            console.log("false")
+        }
+        else setHeart(true)
+        console.log("true")
+    }
+
+    const handleDoubleClick = () =>{
+        setCount(count + 1)
+    }
+
+    return (
+        <div className="wrapper">
+           <h1>My sweetheart</h1>
+           <h2>Counter {count}</h2>
+           <img src={border} alt="" />
+           <div className="container">
+            <RxFace className="white clicker" onDoubleClick={handleDoubleClick}/>
+            {like ? <RxHeartFilled className="red clicker" onClick={handleHeart}/> : <RxHeart className="red clicker" onClick={handleHeart}/>}
+           </div>
+        </div>
+    )
+}
+
+
+const el = document.getElementById('root')
+const root = ReactDOM.createRoot(el)
+root.render(<MyFunction />)
